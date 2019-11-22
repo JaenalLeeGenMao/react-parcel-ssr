9a648511549c58bd4bc26debe1584a6f09b424b2
Jaenal Leegenmao <jaenal.leegenmao@supersoccer.co.id>
2019-11-22 10:37:47 +0700

    add resize listener App.js making isMobile responsive on client side

--- src/App.js
+++ src/App.js
@@ -2,22 +2,40 @@
 import React, { Component } from "react";
 import { withRouter } from "react-router";
 import { Switch, Route } from "react-router-dom";
+import { connect } from "react-redux";
 
 import routes from "./routes";
+
+import { setRuntimeVariable } from "~/store/actions/runtime";
 
 import "~/styles/main.css";
 
 class App extends Component {
   componentDidMount() {
     this.unlisten = this.props.history.listen((location, action) => {
+      /** tracker view pages implement disini */
       console.log("on route change", location);
     });
+    window.addEventListener("resize", this.updateWindowDimensions);
     console.log("App componentDidMount", this.props);
   }
 
   componentWillUnmount() {
     this.unlisten();
+    window.removeEventListener("resize", this.updateWindowDimensions);
   }
+
+  updateWindowDimensions = () => {
+    const { isMobile } = this.props.runtime;
+
+    if (!isMobile && window.innerWidth <= 875) {
+      this.props.setRuntimeVariable({ name: "isMobile", value: true });
+    }
+
+    if (isMobile && window.innerWidth > 875) {
+      this.props.setRuntimeVariable({ name: "isMobile", value: false });
+    }
+  };
 
   render() {
     return (
@@ -35,4 +53,13 @@ class App extends Component {
   }
 }
 
-export default withRouter(App);
+const mapProps = [
+  state => ({
+    runtime: state.runtime
+  }),
+  {
+    setRuntimeVariable
+  }
+];
+
+export default withRouter(connect(...mapProps)(App));
