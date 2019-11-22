ebafb4d7426cebba8a3fa020b7e977e8f0d53018
Jaenal Leegenmao <jaenal.leegenmao@supersoccer.co.id>
2019-11-22 09:19:29 +0700

    add routing listener on App.js

--- src/App.js
+++ src/App.js
@@ -1,24 +1,38 @@
 // src/components/App.js
-import React, { Component } from 'react'
-import { Switch, Route } from 'react-router-dom'
+import React, { Component } from "react";
+import { withRouter } from "react-router";
+import { Switch, Route } from "react-router-dom";
 
-import routes from './routes'
+import routes from "./routes";
 
-import '~/styles/main.css'
+import "~/styles/main.css";
 
 class App extends Component {
-  componentDidMount () {
-    console.log('App componentDidMount')
+  componentDidMount() {
+    this.unlisten = this.props.history.listen((location, action) => {
+      console.log("on route change", location);
+    });
+    console.log("App componentDidMount", this.props);
   }
-  render () {
+
+  componentWillUnmount() {
+    this.unlisten();
+  }
+
+  render() {
     return (
       <Switch>
-        {routes.map(route =>
-          <Route key={route.path} path={route.path} exact={route.exact} component={route.component} />
-        )}
+        {routes.map(route => (
+          <Route
+            key={route.path}
+            path={route.path}
+            exact={route.exact}
+            component={route.component}
+          />
+        ))}
       </Switch>
-    )
+    );
   }
 }
 
-export default App
+export default withRouter(App);
