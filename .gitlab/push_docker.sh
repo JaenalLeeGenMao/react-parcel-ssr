1f69ed3fe415c4e7fae848ef51b3391c23b13208
Jaenal Leegenmao <jaenal.leegenmao@supersoccer.co.id>
2019-12-13 16:11:15 +0700

    apa aja da salahin ical

--- .gitlab/push_docker.sh
+++ .gitlab/push_docker.sh
@@ -3,38 +3,27 @@
 # Set image name
 REPOSITORY_ADDRESS="${REPOSITORY_HOST}/${GCLOUD_PROJECT_ID}/${CI_PROJECT_NAMESPACE}/${CI_PROJECT_NAME}"
 IMAGE_NAME="${REPOSITORY_ADDRESS,,}:${CI_PIPELINE_IID}"
-CACHE_IMAGE="${REPOSITORY_ADDRESS,,}:latest"
+
+# Build Docker!
+docker build \
+  --tag $IMAGE_NAME \
+  --build-arg REACT_APP_ENV \
+  --build-arg NODE_ENV \
+  --build-arg CDN_PATH \
+  .
+
+mkdir /temp-assets
+container_id=$(docker create ${IMAGE_NAME})
+docker cp $container_id:/app/dist/client/. /temp-assets/.
+docker rm $container_id
 
 # Auth gcloud
 gcloud auth configure-docker --quiet
 
-# Try get node_modules cache first
-docker pull $CACHE_IMAGE || true
-
-# Build Docker!
-docker build \
-  --cache-from $CACHE_IMAGE \
-  --tag $IMAGE_NAME \
-  --tag $CACHE_IMAGE \
-  --build-arg REACT_APP_ENV \
-  --build-arg NODE_ENV \
-  --build-arg CDN_PATH \
-  --build-arg VIDEO_ENDPOINT \
-  --build-arg AUTH_ENDPOINT \
-  --build-arg SUBSCRIPTION_API_URL \
-  --build-arg OAUTH_APP_KEY_WEB \
-  --build-arg OAUTH_APP_SECRET_WEB \
-  --build-arg OAUTH_APP_KEY_MOBILE \
-  --build-arg OAUTH_APP_SECRET_MOBILE .
-
-mkdir /temp-assets
-docker_temp=$(docker create ${IMAGE_NAME})
-docker cp $docker_temp:/app/dist/client. /temp-assets/.
-docker rm -v $docker_temp
 gsutil -m -h "Cache-Control:public,max-age=31556952" cp -r /temp-assets/* ${GCS_PATH}/assets/
 gsutil -m -h "Cache-Control:public,max-age=31556952" cp -r src/assets/assets-global/* ${GCS_PATH}/assets-global/
 rm -fr /temp-assets
 
+
 # Push docker image
 docker push $IMAGE_NAME
-docker push $CACHE_IMAGE
